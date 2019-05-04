4d99f51859eba839b05104c3f095f078e9526714
hsjoihs <hs.ioling.hs@gmail.com>
2019-05-04 20:10:05 +0900

    Rename variables; migrate to ts

--- image_table/html_mysterious_encoding_consistency.js
+++ image_table/html_mysterious_encoding_consistency.js
@@ -1,23 +1,19 @@
-function check_constistency(){
-	var arr1 = mysterious_encoding.filter(a => a !== "??").sort();
-	var arr2 = linzi_list.sort();
-
-	if (arr1.length !== arr2.length) {
-		alert("inconsistent: length mismatch");
-		document.write("arr1: " + JSON.stringify(arr1) + "<br>");
-		document.write("arr2: " + JSON.stringify(arr2) + "<br>");
-		return;
-	}
-
-	var c = arr1.map(function(e, i) {
-	  return {a: e, b: arr2[i]};
-	}).filter(q => q.a !== q.b);
-
-	if (c.length !== 0) {
-		alert("inconsistent: content mismatch");
-		document.write("arr1: " + JSON.stringify(arr1) + "<br>");
-		document.write("arr2: " + JSON.stringify(arr2) + "<br>");
-	}
-
-	document.write("consistent.");
+function check_constistency() {
+    var encoded_linzi = mysterious_encoding.filter(a => a !== "??").sort();
+    var sorted_linzi_list = linzi_list.sort();
+    if (encoded_linzi.length !== sorted_linzi_list.length) {
+        alert("inconsistent: length mismatch");
+        document.write("arr1: " + JSON.stringify(encoded_linzi) + "<br>");
+        document.write("arr2: " + JSON.stringify(sorted_linzi_list) + "<br>");
+        return;
+    }
+    var zip_match = encoded_linzi.map(function (e, i) {
+        return e === sorted_linzi_list[i];
+    });
+    if (!zip_match.every(q => q)) { // not everything matches
+        alert("inconsistent: content mismatch");
+        document.write("arr1: " + JSON.stringify(encoded_linzi) + "<br>");
+        document.write("arr2: " + JSON.stringify(sorted_linzi_list) + "<br>");
+    }
+    document.write("consistent.");
 }
---
+++ image_table_ts/html_mysterious_encoding_consistency.ts
@@ -0,0 +1,23 @@
+function check_constistency(): void{
+	var encoded_linzi: string[] = mysterious_encoding.filter(a => a !== "??").sort();
+	var sorted_linzi_list: string[] = linzi_list.sort();
+
+	if (encoded_linzi.length !== sorted_linzi_list.length) {
+		alert("inconsistent: length mismatch");
+		document.write("arr1: " + JSON.stringify(encoded_linzi) + "<br>");
+		document.write("arr2: " + JSON.stringify(sorted_linzi_list) + "<br>");
+		return;
+	}
+
+	var zip_match: boolean[] = encoded_linzi.map(function(e: string, i: number) {
+	  return e === sorted_linzi_list[i];
+	});
+
+	if (!zip_match.every(q => q)) { // not everything matches
+		alert("inconsistent: content mismatch");
+		document.write("arr1: " + JSON.stringify(encoded_linzi) + "<br>");
+		document.write("arr2: " + JSON.stringify(sorted_linzi_list) + "<br>");
+	}
+
+	document.write("consistent.");
+}
