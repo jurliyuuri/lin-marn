2fdace4d6fb5add38ad7bb8ebf1ad67b6ff7ae15
hsjoihs <hs.ioling.hs@gmail.com>
2019-05-04 19:43:18 +0900

    Migrate to TypeScript

--- image_table/html_to_linzi_image.js
+++ image_table/html_to_linzi_image.js
@@ -1,36 +1,37 @@
-var Global = {};
+var Global = {
+    precedence: [],
+    draw: (prec) => { },
+    sy: () => { },
+    jv: () => { },
+    noborder: () => { },
+    border: () => { }
+};
 Global.precedence = ["SY", "jv", "noborder", "border"];
-
-Global.draw = function(prec){
-    document.getElementById("res").innerHTML = 
-        document.getElementById('inputs').value.split(" ")
-        .map(function(txt){
-                var ans = "";
-                for(var i=0; i<txt.length;i++){
-                    ans += getImage(txt[i], prec, 100);
-                }
-                return ans;
-            })
-        .join("<br><br>");
-    
-}
-
-Global.sy = function() {
+Global.draw = function (prec) {
+    document.getElementById("res").innerHTML =
+        (document.getElementById('inputs')).value.split(" ")
+            .map(function (txt) {
+            var ans = "";
+            for (var i = 0; i < txt.length; i++) {
+                ans += getImage(txt[i], prec, 100);
+            }
+            return ans;
+        })
+            .join("<br><br>");
+};
+Global.sy = function () {
     Global.precedence = ["SY", "jv", "noborder", "border"];
     Global.draw(Global.precedence);
-}
-
-Global.jv = function() {
+};
+Global.jv = function () {
     Global.precedence = ["jv", "SY", "noborder", "border"];
     Global.draw(Global.precedence);
-}
-
-Global.noborder = function() {
+};
+Global.noborder = function () {
     Global.precedence = ["noborder", "border", "SY", "jv"];
     Global.draw(Global.precedence);
-}
-
-Global.border = function() {
+};
+Global.border = function () {
     Global.precedence = ["border", "noborder", "SY", "jv"];
     Global.draw(Global.precedence);
-}
+};
---
+++ image_table_ts/html_to_linzi_image.ts
@@ -0,0 +1,51 @@
+type ToLinziImageGlobal = {
+    precedence: ImageAuthor[];
+    draw: (prec: ImageAuthor[]) => void;
+    sy: () => void;
+    jv: () => void;
+    noborder: () => void;
+    border: () => void;
+}
+
+var Global: ToLinziImageGlobal = {
+    precedence: [],
+    draw: (prec) => {},
+    sy: () => {},
+    jv: () => {},
+    noborder: () => {},
+    border: () => {}
+};
+Global.precedence = ["SY", "jv", "noborder", "border"];
+
+Global.draw = function(prec){
+    document.getElementById("res")!.innerHTML = 
+        (<HTMLInputElement>(document.getElementById('inputs')!)).value.split(" ")
+        .map(function(txt: string): string{
+                var ans: string = "";
+                for(var i=0; i<txt.length;i++){
+                    ans += getImage(txt[i], prec, 100);
+                }
+                return ans;
+            })
+        .join("<br><br>");
+}
+
+Global.sy = function() {
+    Global.precedence = ["SY", "jv", "noborder", "border"];
+    Global.draw(Global.precedence);
+}
+
+Global.jv = function() {
+    Global.precedence = ["jv", "SY", "noborder", "border"];
+    Global.draw(Global.precedence);
+}
+
+Global.noborder = function() {
+    Global.precedence = ["noborder", "border", "SY", "jv"];
+    Global.draw(Global.precedence);
+}
+
+Global.border = function() {
+    Global.precedence = ["border", "noborder", "SY", "jv"];
+    Global.draw(Global.precedence);
+}
