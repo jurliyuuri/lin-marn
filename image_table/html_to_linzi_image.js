e2274afd10b032103dd6add0027aa6c36530b596
hsjoihs <hs.ioling.hs@gmail.com>
2019-05-04 19:33:19 +0900

    split html_to_linzi_image.js

---
+++ image_table/html_to_linzi_image.js
@@ -0,0 +1,36 @@
+var Global = {};
+Global.precedence = ["SY", "jv", "noborder", "border"];
+
+Global.draw = function(prec){
+    document.getElementById("res").innerHTML = 
+        document.getElementById('inputs').value.split(" ")
+        .map(function(txt){
+                var ans = "";
+                for(var i=0; i<txt.length;i++){
+                    ans += getImage(txt[i], prec, 100);
+                }
+                return ans;
+            })
+        .join("<br><br>");
+    
+}
+
+Global.sy = function() {
+    Global.precedence = ["SY", "jv", "noborder", "border"];
+    Global.draw(Global.precedence);
+}
+
+Global.jv = function() {
+    Global.precedence = ["jv", "SY", "noborder", "border"];
+    Global.draw(Global.precedence);
+}
+
+Global.noborder = function() {
+    Global.precedence = ["noborder", "border", "SY", "jv"];
+    Global.draw(Global.precedence);
+}
+
+Global.border = function() {
+    Global.precedence = ["border", "noborder", "SY", "jv"];
+    Global.draw(Global.precedence);
+}
