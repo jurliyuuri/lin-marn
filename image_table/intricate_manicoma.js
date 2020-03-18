3e5b106c94bccad3749f09a395d4657132eb1bfd
jekto_vatimeliju <jekto.vatimeliju@gmail.com>
2020-03-18 11:24:49 +0900

    分母に足すべきは2

--- image_table/intricate_manicoma.js
+++ image_table/intricate_manicoma.js
@@ -4,7 +4,7 @@ function isComplexObj(o) {
 const befds = Object.keys(composition2).map(key => composition2[key]);
 const complex_ones = befds.filter(isComplexObj);
 const non_complex_ones = befds.filter(o => !Array.isArray(o.strokeCount));
-if (Math.random() < (complex_ones.length + 1) / (befds.length + 1)) {
+if (Math.random() < (complex_ones.length + 1) / (befds.length + 2)) {
     alert("complex");
 }
 else {
--- image_table_ts/intricate_manicoma.ts
+++ image_table_ts/intricate_manicoma.ts
@@ -25,7 +25,7 @@ const complex_ones: {
     composition: string[]
 }[] = befds.filter(isComplexObj);
 const non_complex_ones = befds.filter(o => !Array.isArray(o.strokeCount));
-if (Math.random() < (complex_ones.length + 1) / (befds.length + 1)) {
+if (Math.random() < (complex_ones.length + 1) / (befds.length + 2)) {
     alert("complex");
 } else {
     alert("not complex");
