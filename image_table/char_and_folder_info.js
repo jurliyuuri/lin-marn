0094586846f116cef8f9f96508e28ebe26e20a50
schwert398 <ritterderschwerter178@gmail.com>
2020-08-24 12:01:07 +0900

    add 「民」, 「網」, 「育」, 「球」, 「橋」, 「産」, 「刺」, 「失」, 「振」, 「須」, 「赤」, 「百」, 「帽」, 「卵」, 「佰」, 「呻」, 「淮」, 「瑪」, 「顫」, 「可」

--- image_table/char_and_folder_info.js
+++ image_table/char_and_folder_info.js
@@ -6,8 +6,8 @@ const linzi_list = allElements()(["我", "汝", "此", "其", "彼", "何", "或
     "振",
     "帽",
     "瑪", "産", "呉", "刺", "育", "呻", "待", "試", "杯", "叮", "可", "檸"]); // REQUIRES CONSTANT UPDATE
-const defined_but_no_image_prepared = ["可", "卵", "淮", "球"];
-const folder_names = allElements()(["燐字画像", "燐字画像2", "燐字画像3", "燐字画像4", "燐字画像5", "燐字画像6", "燐字画像7", "燐字画像8", "燐字画像9", "燐字画像10", "燐字画像11", "燐字画像12", "燐字画像13", "燐字画像14", "燐字画像15", "燐字画像16", "燐字画像17"]);
+const defined_but_no_image_prepared = [];
+const folder_names = allElements()(["燐字画像", "燐字画像2", "燐字画像3", "燐字画像4", "燐字画像5", "燐字画像6", "燐字画像7", "燐字画像8", "燐字画像9", "燐字画像10", "燐字画像11", "燐字画像12", "燐字画像13", "燐字画像14", "燐字画像15", "燐字画像16", "燐字画像17", "燐字画像18", "燐字画像19"]);
 const folder_type = {
     "燐字画像": "SY",
     "燐字画像2": "border",
@@ -25,5 +25,7 @@ const folder_type = {
     "燐字画像14": "border",
     "燐字画像15": "noborder",
     "燐字画像16": "jv",
-    "燐字画像17": "jv touch panel"
+    "燐字画像17": "jv touch panel",
+    "燐字画像18": "border",
+    "燐字画像19": "noborder",
 };
