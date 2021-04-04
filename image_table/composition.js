6bf42f962ae0057d9376d8f4e6e5b32653fa5657
schwert398 <ritterderschwerter178@gmail.com>
2021-04-05 00:13:44 +0900

    add 【遊】

--- image_table/composition.js
+++ image_table/composition.js
@@ -390,4 +390,5 @@ const composition2 = {
     D442: { "isDecomposable": true, "hanzi": "鶏", "composition": ["⿱四鳥"], "strokeCount": ["D22", "D311"] },
     D443: { "isDecomposable": true, "hanzi": "胸", "composition": ["⿻人再"], "strokeCount": ["D4", "D193"] },
     D444: { "isDecomposable": true, "hanzi": "怖", "composition": ["⿻心寒"], "strokeCount": ["D14", "D407"] },
+    D445: { "isDecomposable": true, "hanzi": "遊", "composition": ["⿱草手", "⿱毛撃"], "strokeCount": ["D254", "D7"] },
 };
