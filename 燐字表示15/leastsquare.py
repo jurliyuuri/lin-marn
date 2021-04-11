47b702155246a9de21dcbe8d21cebb9f1f0532a2
sozysozbot <sozysozbot@gmail.com>
2021-04-12 00:42:26 +0900

    Calculate the correct parameter

--- 燐字表示15/leastsquare.py
+++ 燐字表示15/leastsquare.py
@@ -2,7 +2,7 @@ import numpy as np
 
 def main():
 	s = np.array([[1,1,1,1],[0,7,0,7],[0,0,14,14]])
-	y = np.array([[226, 2842,231,2847],[432, 427,4291,4287]])
+	y = np.array([[350, 2966,347,2964],[474, 478,4332,4336]])
 	print(y @ s.T @ np.linalg.inv(s@s.T))
 	
 	
