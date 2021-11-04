c18efdca18e7121ce23f08b3ca9da3838de50f96
jekto_vatimeliju <jekto.vatimeliju@gmail.com>
2021-11-05 03:45:57 +0900

    Implement column hiding; resolves #1

--- image_table/generate_linzi_image_table_html.js
+++ image_table/generate_linzi_image_table_html.js
@@ -146,7 +146,7 @@ function generate_table_narrow_html(id, hidden_columns) {
         })();
         ans += first_cell;
         ans += imageAuthors.map((author, index) => "<td style='text-align: center'>"
-            + ((hidden_columns === null || hidden_columns === void 0 ? void 0 : hidden_columns.includes(author)) || (hidden_columns === null || hidden_columns === void 0 ? void 0 : hidden_columns.includes(`${index}`)) ? "(hidden)" :
+            + ((hidden_columns === null || hidden_columns === void 0 ? void 0 : hidden_columns.includes(author)) || (hidden_columns === null || hidden_columns === void 0 ? void 0 : hidden_columns.includes(`${index}`)) ? `<span style="color: #005242">(hidden)</span>` :
                 folder_names.filter(fname => folder_type[fname] === author).map(name => getImageFromLinziAndFolderIfExists(name, linzi)).join("")) + "</td>").join("");
         ans += "</tr>";
     });
