ff6ac626e7959409064410132d52d6e4c93be324
meloviliju <ritterderschwerter178@gmail.com>
2023-12-26 19:15:13 +0900

    composition.tsのstrokeCountを正しくした

--- header.js
+++ header.js
@@ -9,14 +9,21 @@ function openClose(){
 
 var hs=document.querySelectorAll("h2,h3,h4");
 var length=hs.length;
-var menu="";
+var top=document.querySelector("h1");
+var menu="<h2>目次</h2>";
 var id=0;
+
+if(top){
+	top.id="top";
+	menu+='<a href="#top">↑トップに戻る</a>'
+}
+menu+="<hr>";
 
 function anchor(element){return '<a href="#'+element.id+'">'+element.innerText+'</a><br>'}
 
 for(var i=0;i<length;i++){
 	if(!hs[i].id){
-		hs[i].id="id"+id++;
+		hs[i].id="id"+(i+1);
 	}
 	switch(hs[i].tagName){
 		case "H2":menu+=anchor(hs[i]);break;
@@ -25,3 +32,4 @@ for(var i=0;i<length;i++){
 	}
 }
 document.getElementById("menu").innerHTML=menu;
+window.addEventListener("load",function(e){location.hash=location.hash;});
