4dd8205b650c98b6fe1efd0a7f1b9670e7f39370
hsjoihs / sozysozbot <sozysozbot@gmail.com>
2024-03-01 23:59:52 +0900

    やっていき

---
+++ sync_rounded.bat
@@ -0,0 +1,12 @@
+cd sync_rounded
+
+REM 初回は clone
+REM git clone https://github.com/yasusho/linmarn_font_project
+
+git pull origin master
+
+xcopy linmarn_font_project\rounded\fixed\ all_glyphs\ /s /e /y
+
+node filter_files.js
+
+cd ..
